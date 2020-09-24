d7c0566c51fac6e3c4bd4a968e8d56736ce3d59d
Batmine <amine.hanifa@gmail.com>
2020-09-25 01:41:01 +0200

    First part of my todo app with the use of STATE in order to keep tracking of the inputText and the Todo list

--- basic-todo-app/src/App.js
+++ basic-todo-app/src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // React way to write variable.. (const it's how we declare variable, the setInputText is a function when the inputTextis called the second one is setInputText), we put the useState in a default value ('') when the app upload the input text should be blanc
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]); // with that we can add our list of todo in our State in order to keep track of our list of todos
+
+  const addATodo = (event) => {
+    //a function gonna fire when I click on ADD A TODO
+    event.preventDefault(); // it's saying to not refresh to keep our todo list when added
+    setTodos([...todos, inputText]); // Trick spread operator, we set the Todos and keep everything to an array (array is just a list), and whatever I put in the inputText it's gonna push it in the array
+    setInputText(""); // after we put our text for the todo app, we set the input clear
+    console.log("This is your todo list", [...todos, inputText]);
+  };
   return (
-    <div className="App">
-      <h1>Chocolatine</h1>
+    // BEM naming (app and not App)
+    <div className="app">
+      <h1>Todo app</h1>
+      <form>
+        <input //We take our input and button in a form in order to be able to add the type submit on our button (for entering the input text in todo list with enter) and this disabled={!inputText} in our button is to make our button not working if te inputText is empty
+          value={inputText} // there we set value and onChange (as the user type in) it fires off an event it grab the input text and his value this is why event.target.value
+          onChange={(event) => setInputText(event.target.value)} // ES6 arrow function a mordern approch to write javascript
+          type="text"
+        />
+
+        <button disabled={!inputText} type="submit" onClick={addATodo}>
+          {" "}
+          ADD A TODO
+        </button>
+
+        {todos.map((
+          todo // map is a ES6 function, it's gonna loop through our todo list and gonna return something ( with =>) and show it after that the todo that it take to show it with h2
+        ) => (
+          <h2>{todo}</h2>
+        ))}
+      </form>
     </div>
   );
 }
